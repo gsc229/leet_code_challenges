18a34127f8499369b4e55b8e6b3f5639086a30a1
greg <gsc229@gmail.com>
2020-10-23 16:13:19 -0400

    making git repository

--- longestPalindromeSubStr.js
+++ longestPalindromeSubStr.js
@@ -1,6 +1,6 @@
 var longestPalindrome = function(s) {
     
-  if(s.length = 1){
+  if(s.length === 1){
       return s
   }
   
@@ -8,16 +8,16 @@ var longestPalindrome = function(s) {
   let current = ""
   
   const reversed = s.split("").reverse().join("")
-  
+  console.log({reversed})
   for(let i = 0; i < s.length; i++){
+  
       if(s[i] === reversed[i]){
           current = current + s[i]
       } else {
           highest = current.length > highest.length ? current : highest
           current = ""
       }
-      console.log(s)
-      console.log(s.split("").reverse().join(""))
+      
   }
   
   return highest
